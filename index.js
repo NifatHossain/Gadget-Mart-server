b5e8b4a5d9853c5f9b5a8163adb7f4978dd4a9c6
NifatHossain <nifathossain1480@gmail.com>
2024-05-24 22:41:33 +0600

    Added index.js file

---
+++ index.js
@@ -0,0 +1,22 @@
+let mysql = require('mysql');
+const express = require('express');
+const bodyParser = require('body-parser');
+var cors = require('cors')
+const app = express();
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'nifatassignment2'
+});
+connection.connect(
+    (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('success');
+        }
+    }
+);
